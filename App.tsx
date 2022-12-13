57afe38edfd88ab9e59f6aafe5ec13f5c3db074e
JCLee777 <huanandli@icloud.com>
2022-12-13 17:48:35 +0800

    feat: Update Create Screen

--- App.tsx
+++ App.tsx
@@ -1,31 +1,49 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "./pages/HomeScreen";
 import WelcomeScreen from "./pages/WelcomeScreen";
-import {useState} from "react";
+import { useState } from "react";
 import themeStore from "./store/ThemeStore";
-import {observer} from "mobx-react-lite";
-import {Image} from "react-native";
+import { observer } from "mobx-react-lite";
+import { Image, View } from "react-native";
 import CreateProfileScreen from "./pages/CreateProfileScreen";
 import ProfileFormScreen from './pages/sub/ProfileFormScreen';
+import CreateWalletScreen from './pages/CreateWalletScreen';
 
 const Stack = createNativeStackNavigator();
 
-const App = observer( () =>  {
+const App = observer(() => {
     const [theme] = useState(() => themeStore);
     return (
         <NavigationContainer>
-            <Stack.Navigator  screenOptions={{
+            <Stack.Navigator screenOptions={{
                 headerStyle: theme.currentThemeStyles.header,
                 headerShadowVisible: false
             }}>
-                <Stack.Screen name="Welcome" options={{
-                    headerTitle: () => <Image style={{ height: 62, width: 200, resizeMode: 'cover' }} source={require('./assets/logo.jpg')}></Image>
-                }} component={WelcomeScreen}/>
-                <Stack.Screen name="Home" component={HomeScreen}/>
-                <Stack.Screen name="CreateProfile" options={{ title: 'Create Your Profile', headerBackTitleVisible: false }} component={CreateProfileScreen}/>
-                <Stack.Screen name="ProfileForm" options={{ headerBackTitleVisible: false }} component={ProfileFormScreen}/>
+                <Stack.Screen
+                    name="CreateWallet"
+                    options={{
+                        headerShown: false
+                    }}
+                    component={CreateWalletScreen}
+                />
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen
+                    name="Welcome"
+                    options={{
+                        headerTitle: () => <Image style={{ height: 62, width: 200, resizeMode: 'cover' }} source={require('./assets/logo.jpg')}></Image>
+                    }}
+                    component={WelcomeScreen}
+                />
+                <Stack.Screen
+                    name="CreateProfile"
+                    options={{ title: 'Create Your Profile', headerBackTitleVisible: false }}
+                    component={CreateProfileScreen} />
+                <Stack.Screen
+                    name="ProfileForm"
+                    options={{ headerBackTitleVisible: false }}
+                    component={ProfileFormScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
---
+++ pages/CreateWalletScreen.tsx
@@ -0,0 +1,63 @@
+import { observer } from "mobx-react-lite";
+import * as React from "react";
+import { useState } from "react";
+import themeStore, { Theme } from "../store/ThemeStore";
+import { Text, View, Image, ImageBackground } from "react-native";
+import { NavigationHelpers } from "@react-navigation/native";
+import Button from "../componenets/Button";
+import { buildColumnGap } from "../utils/layout";
+
+const CreateWalletScreen = observer(({ navigation }: { navigation: NavigationHelpers<any> }) => {
+  const [theme] = useState(() => themeStore);
+  return (
+    <View style={theme.currentThemeStyles.page}>
+      {buildColumnGap(52)}
+      <View style={{ flex: 1 }}>
+        <ImageBackground
+          style={{ height: '100%', width: '100%' }}
+          source={require('../assets/icon-create-wallet.png')}
+        />
+      </View>
+
+      <View style={{ padding: 28 }}>
+        {buildColumnGap(20)}
+        <Text style={{ fontSize: 32 }}>
+          New to SuiLove
+        </Text>
+        {buildColumnGap(20)}
+        <Text style={{ fontSize: 16, color: '#988DA2' }}>
+          Create a new wallet or import your existing wallet by 12-word seed phrase.
+        </Text>
+
+        {buildColumnGap(67)}
+
+        <View style={{ height: 44 }}>
+          <Button
+            color="#FF9877"
+            type="normal"
+            text="Create new wallet"
+            onPress={() => {
+              navigation.navigate('CreateProfile')
+            }}></Button>
+        </View>
+
+        {buildColumnGap(20)}
+
+        <View style={{ height: 44 }}>
+          <Button
+            color="#FF9877"
+            type="normal"
+            text="Import new wallet"
+            onPress={() => {
+              navigation.navigate('CreateProfile')
+            }}></Button>
+        </View>
+
+        {buildColumnGap(40)}
+
+      </View>
+    </View>
+  );
+});
+
+export default CreateWalletScreen;
